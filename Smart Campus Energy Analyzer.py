113f74a4fc0f0265e4f5b570d8e12f29b3db26f2
AP24110011751 <kalyangandepalli@gmail.com>
2026-03-17 23:30:01 +0530

    Add files via upload

---
+++ Smart Campus Energy Analyzer.py
@@ -0,0 +1,51 @@
+readings=[]
+n=int(input("Enter how many readings you have:"));
+
+# Reading the Energy readings of different buildings
+for i in range(n):
+    readings.append(int(input(f"Enter Energy reading of building {i+1}:")))
+
+# Categorization based on the values
+categorized_data={}
+categorized_data["efficient"]=[energy for energy in readings if 0<=energy<=50]
+categorized_data["moderate"]=[energy for energy in readings if 51<=energy<=150]
+categorized_data["high"]=[energy for energy in readings if energy>150]
+categorized_data["invalid"]=[energy for energy in readings if energy<0]
+
+# Total Energy Consumption
+
+total_consumpt=0
+for energy in readings:
+    if energy>=0:
+        total_consumpt=total_consumpt+energy
+
+# Counts
+efficient_count = len(categorized_data["efficient"])
+moderate_count = len(categorized_data["moderate"])
+high_count = len(categorized_data["high"])
+
+# Summarizing the Statistics
+if high_count>3:
+    result="Overconsumption"
+
+elif total_consumpt>600:
+    result="Energy Waste Detected"
+
+elif abs(efficient_count-moderate_count)<=1:
+    result="Balanced Usage"
+
+elif efficient_count>moderate_count:
+    result="Efficient Campus"
+
+else:
+    result="Moderate Usage"
+
+stats=(result,)
+
+# Output
+
+print("Categorized Readings:\n")
+print(categorized_data,"\n")
+print(f"Total Energy Consumption:{total_consumpt}\n")
+print(f"Buildings Count:{len(readings)}\n")
+print(f"Efficiency Result:{stats[0]}")
